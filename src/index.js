6f7d02bd6021effe5d5c1ffb89f90fff031f5e53
AlexKolbe <helping5@yandex.ru>
2026-02-22 11:51:34 +0300

    подготовка в следующей домашке

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import timer from './modules/timer'
 import menu from './modules/menu'
 import modal from './modules/modal'
+import validator from './modules/validator'
+
 
 timer('25 february 2026');
 menu();
 modal();
+validator();
 console.log('Hello, webpack!');
 
---
+++ src/modules/validator.js
@@ -0,0 +1,6 @@
+//
+const validator = () => {
+	console.log('validator.js');
+}
+
+export default validator;
